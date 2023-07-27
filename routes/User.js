d5fe7ceb03da14925b378630e256adc592f5988c
puneet0408 <punitvirmani725@gmail.com>
2023-07-28 02:03:18 +0530

    build file added

--- routes/User.js
+++ routes/User.js
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + file.originalname);
-  },
+  }
 });
 
 const upload = multer({ storage: storage });
