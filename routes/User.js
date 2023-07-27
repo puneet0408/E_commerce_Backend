899ede99c295dee52d9d9c1f26009a51e9a3fca1
puneet0408 <punitvirmani725@gmail.com>
2023-07-27 15:30:12 +0530

    build folder

--- routes/User.js
+++ routes/User.js
@@ -1,13 +1,17 @@
 import { fetchUserById, UpdateUser, DeleteUser } from "../controller/User.js";
 import multer from "multer";
-
+import path from "path";
 import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 const UserRouter = express.Router();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-
+const uploadPath = path.join(__dirname, "uploads/");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, `${uploadPath}`);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
