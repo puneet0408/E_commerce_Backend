f89f03b9d26f8e0acfc7f051f46ef8eab74895a2
puneet0408 <punitvirmani725@gmail.com>
2023-07-28 14:29:22 +0530

    peofile Path

--- routes/User.js
+++ routes/User.js
@@ -1,27 +1,25 @@
 import { fetchUserById, UpdateUser, DeleteUser } from "../controller/User.js";
 import multer from "multer";
-import path from "path";
+ import path from "path";
 import express from "express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-
 const UserRouter = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-console.log(__dirname);
-const uploadPath = path.join(__dirname,"uploads/");
+const uploadPath = path.join(__dirname, "../uploads");
 const server = express();
 server.use(express.static(uploadPath));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null,uploadPath );
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + file.originalname);
-  },
+  }
 });
 
 const upload = multer({ storage: storage });
