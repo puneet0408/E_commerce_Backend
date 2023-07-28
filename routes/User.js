8b069aaca4a09a2c7681a25e9f081a6f5ebc991b
puneet0408 <punitvirmani725@gmail.com>
2023-07-28 10:09:15 +0530

    peofile Path

--- routes/User.js
+++ routes/User.js
@@ -14,7 +14,7 @@ server.use(express.static(uploadPath));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
