6551872f2f743009958733b4c6832ea401ce4b99
puneet0408 <punitvirmani725@gmail.com>
2023-07-28 11:45:39 +0530

    peofile Path

--- routes/User.js
+++ routes/User.js
@@ -9,7 +9,7 @@ const UserRouter = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const uploadPath = path.join(__dirname, "uploads");
+const uploadPath = path.join(__dirname, "../uploads");
  
 
 const storage = multer.diskStorage({
