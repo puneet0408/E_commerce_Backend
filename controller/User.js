c70b88821b12d39a141e6b7f75d68d6bf4a9df1d
puneet0408 <punitvirmani725@gmail.com>
2023-07-30 03:30:13 +0530

    cloudinary shift to frontend

--- controller/User.js
+++ controller/User.js
@@ -1,14 +1,4 @@
-
 import { User } from "../model/User.js";
- 
-import {v2 as cloudinary} from 'cloudinary';
-          
-cloudinary.config({ 
-  cloud_name: 'dygbz1kio', 
-  api_key: '621454171282925', 
-  api_secret: '7W20EzMnczOvtBgMihR27WC6X_I' 
-});
-
 
 export const fetchUserById = async (req, res) => {
   const { id } = req.params;
@@ -22,45 +12,28 @@ export const fetchUserById = async (req, res) => {
 
 export const UpdateUser = async (req, res) => {
   console.log(req.body, "body");
-  const { name, addresses } = req.body;
+  const { name, addresses, profilePic } = req.body;
   const { id } = req.params;
-
   try {
-    let profilePicUrl = null;
-
-    if (req.files && req.files.profilePic) {
-      const result = await new Promise((resolve, reject) => {
-        cloudinary.uploader.upload(req.files.profilePic.tempFilePath, (err, result) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(result);
-            console.log(result, "result");
-          }
-        });
-      });
-
-      profilePicUrl = result.url;
+    let imagename = null;
+    if (profilePic) {
+      imagename = profilePic;
     } else {
-      const existingUser = await User.findById(id);
-      profilePicUrl = existingUser.profilePic;
+      const existingImg = await User.findById(id);
+      imagename = existingImg.profilePic;
     }
-
     const doc = await User.findOneAndUpdate(
       { _id: id },
-      { name: name, addresses: addresses, profilePic: profilePicUrl },
+      { name: name, addresses: addresses, profilePic: imagename },
       {
         new: true,
       }
     );
-
     res.status(200).json(doc);
   } catch (err) {
     res.status(400).json(err);
   }
 };
-
-
 
 export const DeleteUser = async (req, res) => {
   const { id } = req.params;
