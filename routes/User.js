003a5d28e00543f67e53a56f706517bb1907b1e9
puneet0408 <punitvirmani725@gmail.com>
2023-07-30 22:15:21 +0530

    logout

--- routes/User.js
+++ routes/User.js
@@ -1,20 +1,6 @@
 import { fetchUserById, UpdateUser, DeleteUser } from "../controller/User.js";
-//import multer from "multer";
 import express from "express";
 const UserRouter = express.Router();
-
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/");
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//     cb(null, uniqueSuffix + file.originalname);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
 
 UserRouter.get("/:id", fetchUserById)
   .patch("/:id", UpdateUser)
